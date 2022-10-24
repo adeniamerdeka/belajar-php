612e19625b697713229284f716789a22b16b99cb
Adenia Merdeka <adeniamerdeka17@gmail.com>
2022-10-24 10:55:26 +0700

    intial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+    echo "Hello World!<br>";
+
+    $nama = "Adenia Merdeka";
+    $umur = 21;
+
+    echo "Nama saya <strong>$nama</strong>, saya berusia $umur tahun.<br>";
+
+    $namaAdik = "Septian";
+    $umurAdik = "13";
+
+    echo "Nama adik saya <strong>$namaAdik</strong>, saya berusia $umurAdik tahun.<br>";
+
+    $selisihUmur = $umur - $umurAdik;
+    echo "selisih usia saya dan adik saya adalah $selisihUmur tahun."
+
+?>
